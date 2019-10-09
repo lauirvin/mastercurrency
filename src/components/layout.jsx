9372fef16cc55cd566d02de67f6c05c70fc822fd
lauirvin <lauirvin98@gmail.com>
2019-10-09 16:26:10 +0800

    Removed Firebase config

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,34 +1,9 @@
 import React, { useEffect } from "react";
 import { Link } from "gatsby";
 
-import withFirebaseAuth from "react-with-firebase-auth";
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "../utilities/firebaseConfig";
 import "../styles/styles.scss";
 
-let firebaseApp;
-let firebaseAppAuth;
-if (!firebase.apps.length) {
-  firebaseApp = firebase.initializeApp(firebaseConfig);
-}
-if (typeof window !== "undefined") {
-  firebaseAppAuth = firebaseApp.auth();
-}
-const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider()
-};
-
-const Layout = ({ children, user, signOut, signInWithGoogle }) => {
-  useEffect(() => {
-    const tag = document.getElementsByTagName("a");
-    Array.from(tag).forEach((element, i) => {
-      if (element.innerHTML === children[0].props.title) {
-        tag[i].classList.add("selected");
-      }
-    });
-  });
-
+const Layout = ({ children }) => {
   return (
     <div id="outer-container">
       <header>
@@ -41,13 +16,6 @@ const Layout = ({ children, user, signOut, signInWithGoogle }) => {
             <Link to="/charts">Charts</Link>
             <Link to="/settings">Settings</Link>
             <Link to="/help">Help</Link>
-            <div className="authentication">
-              {user ? (
-                <a onClick={signOut}>Logout</a>
-              ) : (
-                <a onClick={signInWithGoogle}>Login</a>
-              )}
-            </div>
           </nav>
         </div>
       </header>
@@ -56,7 +24,4 @@ const Layout = ({ children, user, signOut, signInWithGoogle }) => {
   );
 };
 
-export default withFirebaseAuth({
-  providers,
-  firebaseAppAuth
-})(Layout);
+export default Layout;
