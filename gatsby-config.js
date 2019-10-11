af42cc7f6d338904f43fa16cf783e334b7a37ebf
lauirvin <lauirvin98@gmail.com>
2019-10-11 20:52:04 +0800

    Added metaData info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "Title",
-    description: "Enter description",
+    title: "MasterCurrency",
+    description:
+      "Currency conversion web application using exchangeratesapi.io API",
     author: "Irvin Ives Lau",
     url: "https://www.irviniveslau.com",
     social: {
       twitter: "irviniveslau",
       instagram: "irvin.dev",
-      email: "lauirvin98@gmail.com",
+      email: "lauirvin98@gmail.com"
     },
-    imageShare: "./src/media/mastercurrency.png",
+    imageShare: "./src/media/mastercurrency.png"
   },
   plugins: [
     `gatsby-plugin-sass`,
@@ -20,8 +21,8 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "src",
-        path: `${__dirname}/src/`,
-      },
-    },
-  ],
-}
+        path: `${__dirname}/src/`
+      }
+    }
+  ]
+};
